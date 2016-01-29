389dd9e4059e8d121835d3b6bef77e68da1c1aac
Galen W Burghardt <galenburghardt1@Galens-MacBook-Pro.local>
2016-01-29 17:28:22 -0600

    Giving up on Paperclip for now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,10 +36,10 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :bucket => 'elasticbeanstalk-us-west-2-762356920553'
-  }
+#  config.paperclip_defaults = {
+ #   :storage => :s3,
+  #  :bucket => 'elasticbeanstalk-us-west-2-762356920553'
+  #}
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
