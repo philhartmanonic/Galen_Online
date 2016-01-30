d3812be1745a280d992a59f05003236753d78dff
Galen W Burghardt <galenburghardt1@Galens-MacBook-Pro.local>
2016-01-30 02:16:54 -0600

    One more try

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,10 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :bucket => 'elasticbeanstalk-us-west-2-762356920553'
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
+    :s3_credentials => {
+      :bucket => 'elasticbeanstalk-us-west-2-762356920553'
+    }
   }
 
   # Raises error for missing translations
