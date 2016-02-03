d6899f7bf39d3b56398b0680651462ac625d2954
Galen W Burghardt <galen.w.burghardt@gmail.com>
2016-02-03 15:18:43 -0600

    Fuckin a

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, Rails.application.secrets.FACEBOOK_APP_ID, Rails.application.secrets.FACEBOOK_SECRET
-end
