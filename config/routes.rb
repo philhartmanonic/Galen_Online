7d311d8a3f23746d298ea0d04908fecec3acf7a8
Galen W Burghardt <galen.w.burghardt@gmail.com>
2016-02-04 11:27:53 -0600

    Revert "Added Facebook login"
    This reverts commit c7a294a4250599ffff2686c8e1ed9c95eca8ad77.

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :resumes
   root 'welcome#welcome'
-
-  get 'auth/:provider/callback' => 'sessions#create'
-  get 'auth/failure' => redirect('/')
-  get 'signout' => 'sessions#destroy', as: 'signout'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
