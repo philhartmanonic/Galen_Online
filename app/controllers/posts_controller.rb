7e201f3f1cbce641990dcfdd54cffde9812d2c1f
Galen W Burghardt <galen.w.burghardt@gmail.com>
2016-02-09 02:08:19 -0600

    Fixed Post routing problem and added roles#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -97,6 +97,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, user_attributes[:email], comments_attributes[:body, :score], votes_attributes[:up])
+      params.require(:post).permit(:title, :body, user_attributes: [:email], comments_attributes: [:body, :score], votes_attributes: [:up])
     end
 end
