58fc1cb5edcdc0c558065ebec76a2c07fa213006
Galen W Burghardt <galen.w.burghardt@gmail.com>
2016-03-10 17:23:01 -0600

    Syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   :storage => :fog,
   :fog_credentials => {
     :provider => 'AWS',
-    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID']
+    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
   :fog_directory => 'elasticbeanstalk-us-west-2-762356920553',
