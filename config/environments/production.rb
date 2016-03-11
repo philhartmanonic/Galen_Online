116af5df89f7d11f4653da927722d5346d187aeb
Galen W Burghardt <galen.w.burghardt@gmail.com>
2016-03-10 18:03:05 -0600

    Added credentials for Paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
     :storage => :s3,
     :s3_host_name => 's3-us-west-2.amazonaws.com',
     :bucket => 'elasticbeanstalk-us-west-2-762356920553'
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_KEY']
   }
   
 
