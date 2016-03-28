365bb7ab57c1f39272e315b34977ba863453cedc
Galen W Burghardt <galen.w.burghardt@gmail.com>
2016-03-28 16:22:38 -0500

    Fixed typo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class PostsController < ApplicationController
+banbclass PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource :except => [:blog, :show]
 
